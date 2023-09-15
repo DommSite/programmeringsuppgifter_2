076138e755fdd9ab48cb6717f93725ebd5d767d4
DommSite <johan.johansson06@live.se>
2023-09-15 13:01:07 +0200

    Add project files.

---
+++ programmeringsuppgift2.1.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+namespace övning2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad heter din favoritbok?");
+            string favobok = Console.ReadLine();
+            Console.WriteLine("Du älskar " + favobok);
+            Console.ReadKey();
+        }
+    }
+}
